32a0acaccd785d4669218d064ecd29b69fa51719
yeshwanthkonk <37962427+yeshwanthkonk@users.noreply.github.com>
2020-07-25 11:13:07 +0530

    Initial Commit

---
+++ Day-4/typeof.js
@@ -0,0 +1,8 @@
+console.log(typeof(1))
+console.log(typeof(1.1))
+console.log(typeof("1.1"))
+console.log(typeof(true))
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof([]))
+console.log(typeof({}))
