fb0e4ceda7337422cb7bf02ad5e28f207057bd32
yeshwanth konka <Yeshwanth Konka>
2020-07-28 09:59:48 +0530

    CLI-commit

--- week-2/Day-1/OpenCORS-API.js
+++ week-2/Day-1/OpenCORS-API.js
@@ -1,7 +1,6 @@
-
 var request = new XMLHttpRequest()
 var url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
-
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 // request.open('GET',url_string, true )
 // // request.setRequestHeader('Access-Control-Allow-Origin','*')
 // request.send();
@@ -12,6 +11,6 @@ var url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&z
 // console.log(data);
 // }
 
-fetch(url_string, {mode: "no-cors"}) // Call the fetch function passing the url of the API as a parameter
+fetch(proxyUrl + url_string) // Call the fetch function passing the url of the API as a parameter
 .then(res=>res.json())
 .then(data=>console.log(data))
