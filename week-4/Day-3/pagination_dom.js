c760499340bae4f86249dba2da244321f119335d
yeshwanth konka <Yeshwanth Konka>
2020-08-13 08:58:47 +0530

    Pagination with DOM

--- week-4/Day-3/pagination_dom.js
+++ week-4/Day-3/pagination_dom.js
@@ -68,6 +68,10 @@ prev.setAttribute('id', "prev");
 prev.innerText = "Go Back";
 butt_div.append(prev);
 
+var num_div = document.createElement('div');
+num_div.setAttribute('id','num_div');
+butt_div.append(num_div);
+
 var next = document.createElement('button');
 next.setAttribute('onclick', "next_func()");
 next.setAttribute('id', "next");
@@ -88,6 +92,54 @@ var present = 0;
 var itemPerPage = 10;
 var dataRows = [];
 var totalPages;
+
+var a_1 = document.createElement('button');
+a_1.setAttribute("onclick", "func(this)");
+var a_2 = document.createElement('button');
+a_2.setAttribute("onclick", "func(this)");
+var a_3 = document.createElement('button');
+a_3.setAttribute("onclick", "func(this)");
+var a_4 = document.createElement('button');
+a_4.setAttribute("onclick", "func(this)");
+var a_5 = document.createElement('button');
+a_5.setAttribute("onclick", "func(this)");
+
+var num_but = [a_1, a_2, a_3, a_4, a_5];
+
+function showNumber(){
+    let st;
+    let en;
+    if(present-2 >=0 && present+2 <= totalPages-1){
+        st = present-2;
+        en = present+3;
+    }
+    else if(present-2 >= 0){
+        st = present - 2;
+        en = totalPages;
+    }
+    else{
+        st = 0;
+        en = 5;
+    }
+    let but_st=0;
+    var num_div_1 = document.createElement('div');
+    num_div_1.setAttribute('id','num_div');
+    for(let i=st;i<en;i++){
+        if(i == present){
+        num_but[but_st].style = "background-color: darkslategrey;";
+        }
+        else{
+            num_but[but_st].style = "";
+        }
+        num_but[but_st].innerText = i+1;
+        num_but[but_st].id = i+1;
+        num_div_1.append(num_but[but_st]);
+        but_st++;
+    }
+    let old = document.getElementById('num_div');
+    butt_div.replaceChild(num_div_1, old);
+}
+
 function show(){
     let j = present*itemPerPage;
     var tbody = document.createElement('tbody');
@@ -122,6 +174,7 @@ function show(){
         document.getElementById('prev').disabled = false;
         document.getElementById('next').disabled = false;
     }
+    showNumber();
 }
 function createRow(){
     var tr = document.createElement("tr");
@@ -158,4 +211,10 @@ function first_func(){
 function last_func(){
     present = totalPages-1;
     show();
+}
+
+function func(node){
+    present = parseInt(node.id) - 1;
+    show();
+    console.log(node.id);
 }
