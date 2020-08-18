e55bec2de8c4ac9beb6a4c1dad641dbc623c3806
yeshwanth konka <Yeshwanth Konka>
2020-08-18 22:53:39 +0530

    Promise with XMLHttpRequest

--- week-5/Day-2/api_xmlhttp_promise.js
+++ week-5/Day-2/api_xmlhttp_promise.js
@@ -1,8 +1,49 @@
 
-function fetch_rest_api(url){
-    var request = new XMLHttpRequest();
+var cont = document.createElement('div');
+cont.setAttribute("class", "container-fluid");
+var row = document.createElement('div');
+row.setAttribute("class", "row");
+row.style = "margin-left: 30px; margin-top: 20px;";
 
+cont.append(row);
+document.body.append(cont);
+
+function createCOL(name, capital, region, flag){
+    var col = document.createElement('div');
+    col.setAttribute("class", "col-3");
+    col.style = "margin-top: 20px;";
+    var card = document.createElement('div');
+    card.setAttribute("class", "card");
+    card.style = 'width: 18rem;';
+    var img = document.createElement('img');
+    img.src = flag;
+    img.setAttribute("height", "180px");
+    img.setAttribute("class", "card-img-top");
+    var card_body = document.createElement('div');
+    card_body.setAttribute("class", "card-body");
+    var h5 = document.createElement('h5');
+    h5.innerText = name;
+    var ul = document.createElement('ul');
+    ul.setAttribute("class", "list-group list-group-flush");
+    var li1 = document.createElement('li');
+    li1.setAttribute("class", "list-group-item");
+    li1.innerText = "Capital: "+capital;
+    var li2  = document.createElement('li');
+    li2.setAttribute("class", "list-group-item");
+    li2.innerText = "Region: "+region;
+    card_body.append(h5);
+    ul.append(li1, li2);
+    card.append(img, card_body, ul);
+    col.append(card);
+    return col;
+}
+
+function fetch_rest_api(url){
+    
+    var request = new XMLHttpRequest();
+    
     return new Promise((resolve, reject)=>{
+        
         request.open('GET',url, true );
 
         request.onload = function() {
@@ -23,24 +64,16 @@ function fetch_rest_api(url){
             console.log("** An error occurred during the transaction with Network **");
             reject("** An error occurred during the transaction with Network **");
         };
-        
+
         request.send();
     });
 }
 
 
-fetch_rest_api("https://restcountries.eu/rest/v2/all")
+fetch_rest_api("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
 .then((data)=>{
     data.forEach((item)=>{
-        let div = document.createElement('div');
-        let p = document.createElement('p');
-        let img = document.createElement('img');
-        p.innerHTML += "Name: "+item['name'];
-        p.innerHTML += "<br> Capital: "+item['capital'];
-        p.innerHTML += "<br> Region: "+item['region'];
-        img.src = item['flag'];
-        div.append(p, img);
-        document.body.append(div);
+        row.append(createCOL(item['name'], item['capital'], item['region'], item['flag']));
     })
 })
 .catch((Err)=>console.error("Error", Err));
