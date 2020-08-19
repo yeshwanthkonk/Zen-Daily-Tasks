36682be73e98e9e3706718f05aec6cd670e7bfd3
yeshwanth konka <Yeshwanth Konka>
2020-08-19 08:38:38 +0530

    Promise with XMLHttpRequest

--- week-5/Day-2/api_xmlhttp_promise.js
+++ week-5/Day-2/api_xmlhttp_promise.js
@@ -8,17 +8,15 @@ row.style = "margin-left: 30px; margin-top: 20px;";
 cont.append(row);
 document.body.append(cont);
 
-function createCOL(name, capital, region, flag){
+function createCOL(name, capital, region, flag, pop){
     var col = document.createElement('div');
     col.setAttribute("class", "col-3");
     col.style = "margin-top: 20px;";
     var card = document.createElement('div');
     card.setAttribute("class", "card");
     card.style = 'width: 18rem;';
-    var img = document.createElement('img');
-    img.src = flag;
-    img.setAttribute("height", "180px");
-    img.setAttribute("class", "card-img-top");
+    var img = document.createElement('div');
+    img.style = `background-image: url(${flag}); background-size: cover; background-repeat: no-repeat; height: 200px;`
     var card_body = document.createElement('div');
     card_body.setAttribute("class", "card-body");
     var h5 = document.createElement('h5');
@@ -31,8 +29,11 @@ function createCOL(name, capital, region, flag){
     var li2  = document.createElement('li');
     li2.setAttribute("class", "list-group-item");
     li2.innerText = "Region: "+region;
+    var li3  = document.createElement('li');
+    li3.setAttribute("class", "list-group-item");
+    li3.innerText = "Population: "+pop;
     card_body.append(h5);
-    ul.append(li1, li2);
+    ul.append(li1, li2, li3);
     card.append(img, card_body, ul);
     col.append(card);
     return col;
@@ -73,7 +74,7 @@ function fetch_rest_api(url){
 fetch_rest_api("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
 .then((data)=>{
     data.forEach((item)=>{
-        row.append(createCOL(item['name'], item['capital'], item['region'], item['flag']));
+        row.append(createCOL(item['name'], item['capital'], item['region'], item['flag'], item['population']));
     })
 })
 .catch((Err)=>console.error("Error", Err));
