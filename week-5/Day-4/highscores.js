da0bced4c3bdb3981bfcd548f3b07d66c5e2a51d
yeshwanth konka <Yeshwanth Konka>
2020-08-22 12:26:50 +0530

    opentriviaDB for Quiz

--- week-5/Day-4/highscores.js
+++ week-5/Day-4/highscores.js
@@ -18,11 +18,18 @@ var scores = [];
 for(let i=0;i<keys.length;i++){
     scores.push([keys[i], localStorage.getItem(keys[i])]);
 }
-scores.sort((a,b)=>b[1]-a[1]);
+if(scores.length > 0){
+    scores.sort((a,b)=>b[1]-a[1]);
 
-for(let i=0;i<scores.length;i++){
+    for(let i=0;i<scores.length;i++){
+        var h1 = document.createElement('h1');
+        h1.innerText = scores[i][0]+" - "+scores[i][1];
+        btn_div.append(h1);
+    }
+}
+else{
     var h1 = document.createElement('h1');
-    h1.innerText = scores[i][0]+" - "+scores[i][1];;
+    h1.innerText = "No Data to Show";
     btn_div.append(h1);
 }
 
